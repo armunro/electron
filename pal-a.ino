9250748df638b047423a17eda2eb2fa95cac5bfe
Andrew Munro <12575339+armunro@users.noreply.github.com>
2025-02-18 09:10:36 -0600

    Update pal-a.ino

--- pal-a.ino
+++ pal-a.ino
@@ -1,10 +1,3 @@
-/**********************************************************************
-  Filename    : NeoPixel
-  Description : Basic usage of LEDPixel, 
-                Make the strip light up in different colors gradually.
-  Auther      : www.freenove.com
-  Modification: 2023/06/15
-**********************************************************************/
 #include "Freenove_WS2812_Lib_for_ESP32.h"
 
 #define LEDS_COUNT  1   // The number of led
@@ -13,7 +6,7 @@
 
 Freenove_ESP32_WS2812 strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
 
-int m_color[5][3] = { {255, 0, 255}, {255, 0, 255}, {255, 0, 0}, {255, 255, 255} };
+int m_color[5][3] = { {255, 0, 0}, {0, 255, 0}, {0, 0, 255}, {255, 255, 255} };
 int delayval = 500;
 
 void setup() {
@@ -23,10 +16,10 @@ void setup() {
 void loop() {
 	for (int j = 0; j < 4; j++) {
 		for (int i = 0; i < LEDS_COUNT; i++) {
-			strip.setLedColorData(i, m_color[j][0], m_color[j][1], m_color[j][2]);// Set color data.
-			strip.show();   // Send color data to LED, and display.
-			delay(delayval);// Interval time of each LED.
+			strip.setLedColorData(i, m_color[j][0], m_color[j][1], m_color[j][2]);
+			strip.show(); 
+			delay(delayval)
 		}
-		delay(200);       // Interval time of each group of colors.
+		delay(200);  
 	}
 }
